9c103221fb9752cbc6a7e9ceaf44cfcb2844f34f
shintia_dharma <shintia.ds@icloud.com>
2024-09-10 12:50:41 +0700

    memperbaiki code html

--- main/views.py
+++ main/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name': 'Strawberry Cheesecake',
-        'size': '30x20',
-        'price': 'Rp647.900',
-        'description': 'A party special that is loved by all. Vanilla flavored baked cheesecake with freshly strawberries.',
-        'notes': '*Customizable for your loved ones special occasion!*'
+        'name': 'Shintia Dharma Shanty',
+        'kelas': 'PBP F',
+        'items': [
+            {'name': 'Strawberry Cheesecake', 'size': '30x20', 'price': 'RP647.900', 'description': 'A party special that is loved by all. Vanilla flavored baked cheesecake with freshly picked strawberries.', 'notes': 'Customizable for your loved ones special occasion!'},
+            {'name': 'Blueberry Cheesecake', 'size': '30x20', 'price': 'RP616.000', 'description': 'Our famous cheesecake pairs perfectly with natural blueberries.', 'notes': 'Customizable for your loved ones special occasion!'},
+        ]
     }
 
     return render(request, 'main.html', context)
