45f20e58bf56287f35d6d1ca22f765f5255e44bd
shintia_dharma <shintia.ds@icloud.com>
2024-09-15 19:50:02 +0700

    revisi

--- main/models.py
+++ main/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Cheesecake(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255)
     size = models.CharField(max_length=10)
     price = models.IntegerField()
