fecbb7a229c10dbd214e5e51b237097edf180a63
shintia_dharma <shintia.ds@icloud.com>
2024-09-17 15:17:02 +0700

    bismillhah last

--- main/models.py
+++ main/models.py
@@ -5,6 +5,6 @@ class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     size = models.CharField(max_length=10, default="Medium") 
-    price = models.IntegerField()
-    description = models.TextField(default="No description")
+    price = models.IntegerField(default=0)  
+    description = models.TextField(default="No description") 
     notes = models.TextField()
