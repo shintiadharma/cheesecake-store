0750ec6450b9e0701780e6ef1a30a1bf2170cdf4
shintia_dharma <shintia.ds@icloud.com>
2024-09-17 14:18:25 +0700

    memperbaiki

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ import uuid
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    size = models.CharField(max_length=10)
-    price = models.IntegerField()
-    description = models.TextField()
+    size = models.CharField(max_length=10, default='')
+    price = models.IntegerField(default='')
+    description = models.TextField(default='')
     notes = models.TextField()
